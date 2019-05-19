1ab1f4ab0fadb4006308df6f9175436c6e8b5acc
errolgh <ewatsonrei@gmail.com>
2019-05-19 11:03:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 // Your code goes here
+//second
 document.addEventListener("DOMContentLoaded", function() {
   console.log("The DOM has loaded");
 });
 
+
+//first
 console.log(
   "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
 );
 
-
+//third
 document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("text").innerHTML = "This is really cool!"
+  console.log("This should be the last console.log().")
 });
