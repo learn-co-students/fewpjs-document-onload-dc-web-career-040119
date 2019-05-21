9398d254ab673d899c0fab57ac11b73109733758
NosaPrecious <Okundaye.preciousnosa@gmail.com>
2019-05-20 22:28:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+console.log("Loaded")
+
+document.addEventListener("DOMContentLoaded", function(){
+changeDisplayText()
+});
+
+function changeDisplayText(){
+  let p = document.querySelector("#text")
+  p.innerHTML = "This is really cool!"
+}
